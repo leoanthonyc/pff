bae93b3cc5b7161622fe9489456bef8f2ba79b7c
Leo Catane <leoanthony.catane@protonmail.com>
2020-12-14 18:59:58 +0800

    Implement edit & delete transactions

--- app/javascript/packs/components/transaction/Transactions.js
+++ app/javascript/packs/components/transaction/Transactions.js
@@ -1,11 +1,77 @@
 import React, { useState } from "react";
-import { useQuery } from "@apollo/client";
-import { TRANSACTIONS_QUERY } from "../../api/transaction";
+import { useQuery, useMutation } from "@apollo/client";
+import {
+  DELETE_TRANSACTION_MUTATION,
+  SAVE_TRANSACTION_MUTATION,
+  TRANSACTIONS_QUERY,
+} from "../../api/transaction";
 import NewTransaction from "./NewTransaction";
 
 const Transaction = ({ transaction }) => {
   const [name, setName] = useState(transaction.name);
-  return <div>{name}</div>;
+  const [editing, setEditing] = useState(false);
+  const [saveTransaction] = useMutation(SAVE_TRANSACTION_MUTATION);
+  const [deleteTransaction] = useMutation(DELETE_TRANSACTION_MUTATION, {
+    update(cache) {
+      cache.modify({
+        fields: {
+          transactions(existingTransactions = [], { readField }) {
+            return existingTransactions.filter(
+              (ref) => readField("id", ref) !== transaction.id
+            );
+          },
+        },
+      });
+    },
+  });
+
+  const handleDelete = async () => {
+    await deleteTransaction({ variables: { id: transaction.id } });
+    setEditing(false);
+  };
+
+  const handleSave = async () => {
+    await saveTransaction({
+      variables: {
+        id: transaction.id,
+        name: name,
+        categoryId: transaction.category.id,
+        accountId: transaction.account.id,
+      },
+    });
+    setEditing(false);
+  };
+  return (
+    <>
+      {editing ? (
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="button" onClick={() => handleSave()}>
+            Save
+          </button>
+          <button type="button" onClick={() => handleDelete()}>
+            Delete
+          </button>
+          <button type="button" onClick={() => setEditing(false)}>
+            Cancel
+          </button>
+        </div>
+      ) : (
+        <div>
+          <div>
+            <strong>{name}</strong>
+            <button type="button" onClick={() => setEditing(true)}>
+              Edit
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 const Transactions = () => {
